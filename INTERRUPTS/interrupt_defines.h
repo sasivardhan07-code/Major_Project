d854cf7f579ca61e0a510f20d5d6f3f915d0d291
Garlapati Sasi Vardhan <garlapatisasi9848@gmail.com>
2026-08-01 22:31:39 +0530

    Create interrupt_defines.h

---
+++ INTERRUPTS/interrupt_defines.h
@@ -0,0 +1,8 @@
+#define EINT0_0_1       0x0000000C    // Configure P0.1 as External Interrupt 0 (EINT0)
+#define EINT0_VIC_CHNO  14            // VIC channel number assigned to EINT0
+
+#define EINT1_0_3       0x000000C0    // Configure P0.3 as External Interrupt 1 (EINT1)
+#define EINT1_VIC_CHNO  15            // VIC channel number assigned to EINT1
+
+#define EINT2_0_7       0x0000C000    // Configure P0.7 as External Interrupt 2 (EINT2)
+#define EINT2_VIC_CHNO  16            // VIC channel number assigned to EINT2
