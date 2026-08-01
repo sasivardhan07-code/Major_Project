ef1b4363b738e7018010b7fbe1b91a5ecfbee3c6
Garlapati Sasi Vardhan <garlapatisasi9848@gmail.com>
2026-08-01 22:39:06 +0530

    Create mma7660_defines.h

---
+++ MMA7660(Accelerometer)/mma7660_defines.h
@@ -0,0 +1,14 @@
+#include "types.h"
+
+#define MMA7660_SA    0x98
+#define MMA7660_SA_R  0x99
+
+#define MMA_XOUT   0x00
+#define MMA_YOUT   0x01
+#define MMA_ZOUT   0x02
+#define MMA_TILT   0x03
+#define MMA_SRST   0x04
+#define MMA_SPCNT  0x05
+#define MMA_INTSU  0x06
+#define MMA_MODE   0x07
+#define MMA_SR     0x08
